e09f89b2ff93ba2eeb06515bcfde203cfb129035
Andras Bekesi <andras.bekesi@bikeonet.hu>
2018-01-16 10:21:30 +0100

    refresh data periodically

--- web-client/src/js/appController.js
+++ web-client/src/js/appController.js
@@ -34,39 +34,45 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojgauge', 'Db
       var smQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
       self.smScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
 
-      // Header
-      // Application Name used in Branding Area
-
-	$.getJSON(apiEndpoint+"/api/ccq").
-              then(function (data) {
-		  console.log(data);
-		  self.ccq(data.ccq / 10);
-              });
-
-
-	$.getJSON(apiEndpoint+"/api/signal").
-              then(function (data) {
-      console.log(data);
-      self.signalLabel(data.signal);
-		  self.signal(100 - (data.signal * -1));
-              });
-
-  $.getJSON(apiEndpoint+"/api/tx_rate").
-              then(function (data) {
-		  console.log(data);
-		  self.tx(parseInt(data.tx_rate));
-              });
-
-  $.getJSON(apiEndpoint+"/api/rx_rate").
-              then(function (data) {
-  	  console.log(data);
-  	  self.rx(parseInt(data.rx_rate));
-              });
-
-
       self.appName = ko.observable("CCQ Web Client");
       // User Info used in Global Navigation area
-      self.userLogin = ko.observable("john.hancock@oracle.com");
+      // self.userLogin = ko.observable("john.hancock@oracle.com");
+
+      //query the api for now and set up a timer to do so in every minute
+      queryUbntApi();
+      window.setInterval(function () {
+        queryUbntApi();
+      }, 60000);
+
+      //collect data
+      function queryUbntApi() {
+        $.getJSON(apiEndpoint+"/api/ccq").
+                    then(function (data) {
+      		  console.log(data);
+      		  self.ccq(data.ccq / 10);
+                    });
+
+
+      	$.getJSON(apiEndpoint+"/api/signal").
+                    then(function (data) {
+            console.log(data);
+            self.signalLabel(data.signal);
+      		  self.signal(100 - (data.signal * -1));
+                    });
+
+        $.getJSON(apiEndpoint+"/api/tx_rate").
+                    then(function (data) {
+      		  console.log(data);
+      		  self.tx(parseInt(data.tx_rate));
+                    });
+
+        $.getJSON(apiEndpoint+"/api/rx_rate").
+                    then(function (data) {
+        	  console.log(data);
+        	  self.rx(parseInt(data.rx_rate));
+                    });
+      }
+
 
       // Footer
       function footerLink(name, id, linkTarget) {
@@ -74,6 +80,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojgauge', 'Db
         this.linkId = id;
         this.linkTarget = linkTarget;
       }
+
       self.footerLinks = ko.observableArray([
         new footerLink('About Oracle', 'aboutOracle', 'http://www.oracle.com/us/corporate/index.html#menu-about'),
         new footerLink('Contact Us', 'contactUs', 'http://www.oracle.com/us/corporate/contact/index.html'),
